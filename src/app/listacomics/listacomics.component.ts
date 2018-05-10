d7cc0f69002552fbac1abaf742e26268f15f70ae
DESKTOP-LO788SI\Juan Pablo <jupa.1996@hotmail.com>
2018-05-10 00:11:17 -0500

    cuadros para la información del comic

--- src/app/listacomics/listacomics.component.ts
+++ src/app/listacomics/listacomics.component.ts
@@ -9,6 +9,9 @@ import { PeticionService } from '../services/peticion.service';
 })
 export class ListacomicsComponent implements OnInit {
 
+  public lista_comics;
+  public likes_comics:Array<number>;
+
   constructor(
     private _peticionService: PeticionService
   ) { }
@@ -18,14 +21,34 @@ export class ListacomicsComponent implements OnInit {
      this._peticionService.getArticulos().subscribe(
       result => {
         console.log(result);
-        // this.lista_pokemones = result;
-
+        this.lista_comics = result.data.results;
+        console.log(this.lista_comics);
+        this.inicializarArray();
+        console.log(this.likes_comics);
       },
       error => {
         var errorMsj = <any>error;
         console.log(errorMsj);
       }
     );
+    
   }
 
-}
+  inicializarArray(){
+    if(this.lista_comics){
+      this.likes_comics = [];
+      for(var i=0; i < Object.keys(this.lista_comics).length;i++){
+        this.likes_comics[i] = 0;
+      }
+    }
+  }
+  
+  gestionarLike(index, okLike) {
+    if (okLike) {
+      this.likes_comics[index] += 1;
+    } else {
+      this.likes_comics[index] -= 1;
+    }
+  }
+}  
+
